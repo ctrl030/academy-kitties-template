a2d790acce617f191bc32a0a32be12bf2812aaa0
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2020-11-17 00:23:33 -0300

    7

--- contracts/Monkeycontract.sol
+++ contracts/Monkeycontract.sol
@@ -29,16 +29,18 @@ contract MonkeyContract is IERC721 {
   // a mapping to store each address's number of Crypto Monkeys - will be queried by balanceOf function - must update at minting (seems done) and transfer (seems done)
   mapping(address => uint256) private _numberOfCMOsOfAddressMapping;
 
-
-  /* trying it with one mapping only
   // mapping of all the tokenIds and their ownerssss - will be queried by ownerOf function - seems done
   mapping (uint256 => address) private _monkeyIdsAndTheirOwnersMapping;
-  */
   
+  // mapping of the allowed addresses for a piece - entry must be deleted when CMO is transfered - seems done
+  mapping (uint256 => address) private _CMO2AllowedAddressMapping;
+
+
+  /* older version, trying it easier
   // mapping of all the tokenIds and their owners and their allowedAddress;
   mapping (uint256 => mapping (address => address)) private _CMO2owner2allowedAddressMapping;
 
-  /* older version, trying it easier
+  
   // mapping, so that each address can have a list of their CMOs and inside that an address, that is allowed to take/move that CMO
   mapping (address => mapping (uint256 => address)) private _address2CMO2allowedAddressMapping;
   */
@@ -52,7 +54,7 @@ contract MonkeyContract is IERC721 {
   event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
 
   // Approval event, emit after successful approval with these parameters  - implement "you can transfer my CMO - functionality"
-  // event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+  event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
 
   // Minted event, emit after successful minting with these parameters  -  seems done
   event Minted (address owner, uint256 tokenId);
@@ -73,8 +75,10 @@ contract MonkeyContract is IERC721 {
   function approve(uint256 tokenId, address allowedAddress) public {   
 
     // requires that the msg.sender is the owner of the CMO to be moved
-    require (_CMO2owner2allowedAddressMapping[tokenId] == msg.sender);
-    _CMO2owner2allowedAddressMapping[tokenId][msg.sender] = allowedAddress;
+    require (_monkeyIdsAndTheirOwnersMapping[tokenId] == msg.sender);
+
+    // stores the allowed address into the mapping for it, with the monkey being the key
+    _CMO2AllowedAddressMapping[tokenId] = allowedAddress;
 
     emit Approval(msg.sender, allowedAddress, tokenId); 
 
@@ -107,15 +111,15 @@ contract MonkeyContract is IERC721 {
     return _numberOfCMOsOfAddressMapping[owner];
   }
   
-  // returns the owner of given tokenId, which is stored in the _CMO2owner2allowedAddressMapping at the [tokenId] position - seems done
+  // returns the owner of given tokenId, which is stored in the _monkeyIdsAndTheirOwnersMapping at the [tokenId] position - seems done
   function ownerOf(uint256 tokenId) external view returns (address){    
-    return _CMO2owner2allowedAddressMapping[tokenId];
+    return _monkeyIdsAndTheirOwnersMapping[tokenId];
   }
 
   /*  need mint function.. needs to generate new ERC721, i.e.  - how can I store the ETH amount that was paid in the event I emit? what data type is ether?
   generate tokenId, - seems done
   store it and assign it to the owner in a mapping  - seems done
-  make transferable  - seems done, just change owner via transfer function in the _CMO2owner2allowedAddressMapping
+  make transferable  - seems done, just change owner via transfer function in the _monkeyIdsAndTheirOwnersMapping
   emit Minted - seems done
  */
   function mint () public {  
@@ -125,17 +129,17 @@ contract MonkeyContract is IERC721 {
     // ... we set the tokenId for this token that is being minted to the amount of the _totalSupply (first CMO will have tokenId = 0, and so on) and then..
     tokenId = _totalSupply;
 
-    // ... save this tokenId to te _CMO2owner2allowedAddressMapping where we keep all of the tokenIds, 
+    // ... save this tokenId to te _monkeyIdsAndTheirOwnersMapping where we keep all of the tokenIds, 
     // and assign and owner to them, which at the moment of minting is the msg.sender
-    _CMO2owner2allowedAddressMapping[tokenId] = msg.sender;
+    _monkeyIdsAndTheirOwnersMapping[tokenId] = msg.sender;
 
-    // then we add 1 to the _numberOfCMOsOfAddressMapping, where we store all the addresses that own Crypto Monkeys and the amount of them
+    // adds 1 to the _numberOfCMOsOfAddressMapping, where we store all the addresses that own Crypto Monkeys and the amount of them
     _numberOfCMOsOfAddressMapping[msg.sender].add(1);
 
     // we update the _totalSupply (as we use this variable to generate the tokenIds, this will never go down, because that would create double ids, etc. 
     // So if we want a burning function, just transfer to a burn address, will still count towards totalSupply.  
-    // We could also create another uint256 that keeps track of how many  CMOs are in that burn address, 
-    // and then substract that number from totalSupply, so that we know how many are in circulation, unburned. Or similar.)
+    // We could also create another uint256 that keeps track of how many CMOs are in that burn address, 
+    // and then substract that number from totalSupply into another uint256 variable, so that we know how many are in circulation, unburned. Or similar.)
     _totalSupply = _totalSupply.add(1);
 
     // emitting the Minted event, logging the minting address (msg.sender) and the tokenId of the CMO that was minted
@@ -148,7 +152,7 @@ contract MonkeyContract is IERC721 {
   * Requirements:
   *
   * - `to` cannot be the zero address. - seems done
-  * - `to` can not be the contract address. - How do I find contract address? Where do we store it?
+  * - `to` can not be the contract address.  seems done
   * - `tokenId` token must be owned by `msg.sender`. - seems done
   *
   * Emits a {Transfer} event.
@@ -159,32 +163,32 @@ contract MonkeyContract is IERC721 {
     //`to` cannot be the zero address. 
     require (to != address(0));
 
-    // FIX, need contract address / where to find it after generated
-    // require (to != );
+    // to` can not be the contract address.
+    require (to != address(this));
 
     // `tokenId` token must be owned by `msg.sender`
-    require (_CMO2owner2allowedAddressMapping[tokenId] == msg.sender);
+    require (_monkeyIdsAndTheirOwnersMapping[tokenId] == msg.sender);
 
-    try {
-      // transferring, i.e. changing ownership entry in the _CMO2owner2allowedAddressMapping for the tokenId
-    _CMO2owner2allowedAddressMapping[tokenId] = to;
-    _CMO2owner2allowedAddressMapping[tokenId][to] = to;
+    // try {
+      // deleting any allowed address for the transfered CMO
+      delete _CMO2AllowedAddressMapping[tokenId];
 
-    // updating "balance" of address in _numberOfCMOsOfAddressMapping, 'to' address has 1 CMO more
-    _numberOfCMOsOfAddressMapping[to].add(1);
+      // transferring, i.e. changing ownership entry in the _monkeyIdsAndTheirOwnersMapping for the tokenId
+      _monkeyIdsAndTheirOwnersMapping[tokenId] = to;  
 
-    // updating "balance" of address in _numberOfCMOsOfAddressMapping, sender has 1 CMO less
-    _numberOfCMOsOfAddressMapping[msg.sender].sub(1);
+      // updating "balance" of address in _numberOfCMOsOfAddressMapping, 'to' address has 1 CMO more
+      _numberOfCMOsOfAddressMapping[to].add(1);
 
-    }
+      // updating "balance" of address in _numberOfCMOsOfAddressMapping, sender has 1 CMO less
+      _numberOfCMOsOfAddressMapping[msg.sender].sub(1);
+    //}
     
+    //catch (err) {
+    //  console.log("Error is: " + err);
+    //}
 
     emit Transfer (msg.sender, to, tokenId);
 
   } 
     
-
-
-
-
 }
